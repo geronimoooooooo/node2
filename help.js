443e20f27b65983f6d38e20d8358a3a3d19ea111
Bro <bro@gmail.com>
2024-03-20 15:26:54 +0100

    update with crtkey

--- help.js
+++ help.js
@@ -7,17 +7,56 @@ console.log(now.toISOString().replace(/[.]\d+/, ''));
 console.log(now.toISOString().replace('T', ' ').replace(/[.]\d+/, ''));
 
 
-
+import express from "express";
 // const https = require('https');
 import https from "https"
 // const fs = require('fs');
 import fs from "fs"
+const app = express()
+import * as dotenv from "dotenv"; 
+dotenv.config();
 
-const options = {
-  pfx: fs.readFileSync('sslcert/STAR_researchstudio_at.pfx')
-};
+app.get('/', (req, res) => {  
+  res.send('startseite');
+  //res.json({ success: true })
+});
 
-https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-}).listen(8000);
+app.get('/home', (req, res) => {
+  console.log("logger in app.get home");    
+  res.send('Home Page');
+});
+
+// const options = {
+//   pfx: fs.readFileSync('sslcert/STAR_researchstudio_at.pfx')
+// };
+
+// const httpsServer = https.createServer({
+//     key: fs.readFileSync('privateKey.key'),
+//     cert: fs.readFileSync('certificate.crt'),
+//   }, app);
+
+ var privateKey  = fs.readFileSync('sslcert/researchstudio_at.key', 'utf8');
+ var certificate = fs.readFileSync('sslcert/STAR_researchstudio_at.crt', 'utf8');
+
+ var credentials = {key: privateKey, cert: certificate};
+
+// const credentials = {
+//   pfx: fs.readFileSync('sslcert/STAR_researchstudio_at.pfx')
+// };
+
+// var httpsServer = https.createServer(credentials, app);
+
+// https.createServer(options, (req, res) => {
+//   res.writeHead(200);
+//   res.send('hello world\n');
+// }).listen(8000);
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(process.env.PORTHTTPS, (err) => {
+  if(err){
+    console.log(new Date().toISOString()+` https server could not start on port: ${process.env.PORTHTTPS}`);
+  }else{
+    console.log(new Date().toISOString()+` https server running on port: ${process.env.PORTHTTPS}`);
+  }
+    });
